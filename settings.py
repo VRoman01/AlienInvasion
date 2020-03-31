d3f3e783c305efb19d7cfcb6e80ef813eff41492
roman <roman@exaption.ru>
2020-03-31 18:54:03 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,23 @@
+class Settings:
+
+    def __init__(self):
+        # Screen settings
+        self.screen_width = 1000
+        self.screen_height = 700
+        self.bg_color = (30, 230, 230)
+
+        # Ship settings
+        self.ship_speed_factor = 0.5
+        self.ship_size = (50, 70)
+
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 5
+        self.fleet_direction = 1
+
+        # Bullet settings
+        self.bullet_speed_factor = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 10
---
+++ ship.py
@@ -0,0 +1,37 @@
+import pygame
+
+
+class Ship:
+
+    def __init__(self, ai_settings, screen):
+        self.screen = screen
+        self.ai_settings = ai_settings
+        self.image = pygame.transform.scale(pygame.image.load('images/alienblaster.png'), ai_settings.ship_size)
+        self.screen_rect = screen.get_rect()
+
+
+        self.moving_up = False
+        self.moving_down = False
+        self.moving_right = False
+        self.moving_left = False
+
+        self.rect = self.image.get_rect()
+        self.rect.bottom = self.screen_rect.bottom
+        self.rect.centerx = self.screen_rect.centerx
+        self.center = float(self.rect.centerx)
+        self.bottom = float(self.rect.bottom)
+
+    def update(self):
+        if self.moving_up and self.rect.y > self.screen_rect.y:
+            self.bottom -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.bottom += self.ai_settings.ship_speed_factor
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
+        self.rect.bottom = self.bottom
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
